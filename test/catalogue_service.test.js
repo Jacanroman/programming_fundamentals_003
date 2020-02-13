602018606fa8aa43afda247aaa7d19ac2668ffa9
Jacanroman <jacanroman@gmail.com>
2020-02-13 15:19:27 +0000

    taks 003

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,107 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Assassination of Margaret Thatcher"
+        )
+      ).toBe(true);
+    });
+
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Chronicles of Narnia"
+        )
+      ).toBe(false);
+    });
+  });
+
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the total number of books which start with a given letter", () => {
+      expect(
+        catalogueService.countBooksByFirstLetter(
+          "W"
+        )
+      ).toBe(2);
+    });
+    
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.countBooksByFirstLetter(
+          "b"
+        )
+      ).toBe(3);
+    });
+    
+  });
+
+
+
+  describe("catalogueService.getQuantity", () => {
+    test("returns the stocks of books  which  with a given title", () => {
+      expect(
+        catalogueService.getQuantity(
+          "The Catcher in the Rye"
+        )
+      ).toBe(10);
+    });
+    
+    
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.getQuantity(
+          "2666"
+        )
+      ).toBe(17);
+    });
+    
+    
+  });
+
+
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns the stocks of books  which  with a given title", () => {
+      expect(
+        catalogueService.getBooksByAuthor(
+          "Robert Bolaño"
+        )
+      ).toEqual(
+        [
+          { title: "2666", author: "Robert Bolaño", quantity: 17 },
+          { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }
+        ]
+
+        );
+    });
+    
+  });
+
+
+  describe("catalogueService.checkQuantity", () => {
+    test("returns true if in stock or false if not", () => {
+      expect(
+        catalogueService.checkQuantity(
+          "By Night In Chile", 4
+        )
+      ).toBe(true);
+    });
+    
+
+   test("returns true if in stock or false if not", () => {
+    expect(
+      catalogueService.checkQuantity(
+        "By Night In Chile", 100
+      )
+    ).toBe(false);
+    });
+
+  });
+  
+
+
 });
